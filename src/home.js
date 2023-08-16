b0e31fed340ff4e47e6c2d28aef694076b45802b
Anuj Johri <anujjohri7@gmail.com>
2023-08-16 00:04:24 -0400

    Home component in progress, added profile photo

--- src/home.js
+++ src/home.js
@@ -1,8 +1,27 @@
+import IMG_3559 from './IMG_3559.jpeg';
+
 function Home () {
     return (
-    <p>Greetings!! Hope you are doing well and thank you for taking time out to know me a little better.
-    </p>
+        <div style={styles.div}>
+            <p>Greetings!! Hope you are doing well and thank you for taking time out 
+                to know me a little better.</p>
+            {/* <img source={require('./IMG_3559.jpeg')} style={styles.img}></img> */}
+            <img src={IMG_3559} alt={'Profile Photo'} style={styles.img}></img>
+        </div>
     ); 
+}
+
+const styles = {
+
+    div : {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+
+    img : {
+        width:'50%',
+        height: '50%',
+    }
 }
 
 export default Home;
