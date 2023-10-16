c0e6d7b2d39c14e2443b7365b35e2c54032a256f
Anuj Johri <anujjohri7@gmail.com>
2023-10-16 00:16:54 -0400

    Added process.env.PUBLIC_URL to src of images in skills file.

--- src/Skills.js
+++ src/Skills.js
@@ -6,99 +6,99 @@ const Skills = () => {
     <div className='skill-container'>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/c.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/c.png'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/java.png' alt='java language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/java.png'} alt='java language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/python.png' alt='python language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/python.png'} alt='python language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/css.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/css.png'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/d3.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/d3.png'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/django.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/django.png'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/flask.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/flask.png'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/gbigquery.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/gbigquery.png'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/git.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/git.png'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/html.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/html.png'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/intellij.jpeg' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/intellij.jpeg'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/jira.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/jira.png'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/js.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/js.png'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/latex.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/latex.png'} alt='c language'></img>
       </div>
 
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/matplotlib.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/matplotlib.png'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/mssql.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/mssql.png'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/mysql.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/mysql.png'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/oracle.jpeg' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/oracle.jpeg'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/pandas.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/pandas.png'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/react.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/react.png'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/react_native.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/react_native.png'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/scikit.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/scikit.png'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/scipy.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/scipy.png'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/seaborn.jpeg' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/seaborn.jpeg'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/snow.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/snow.png'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/SQL.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/SQL.png'} alt='c language'></img>
       </div>
       <div className='child-container'>
-      <img className='image' src='./Personal_Website/skills_logos/vscode.png' alt='c language'></img>
+      <img className='image' src={process.env.PUBLIC_URL + '/skills_logos/vscode.png'} alt='c language'></img>
       </div>
     </div>
   )
